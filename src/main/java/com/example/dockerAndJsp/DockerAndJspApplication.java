7650292d0a82754198621a535cc788d0324067ad
Genz, Nico <nico.genz@sybit.de>
2019-07-30 10:14:34 +0200

    project setup

---
+++ src/main/java/com/example/dockerAndJsp/DockerAndJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerAndJsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAndJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAndJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerAndJsp/DockerAndJspApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.dockerAndJsp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerAndJspApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
